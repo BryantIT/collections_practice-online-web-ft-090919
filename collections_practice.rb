963f7b64ad35b291abc49fb3cea06af2b6586c34
Bryant Richards <dragonseyeimages@gmail.com>
2019-09-14 16:10:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,3 +13,11 @@ end
 def swap_elements(array)
   array[0], array[1], array[2] = array[0], array[2], array[1]
 end
+
+def reverse_array
+  array.reverse
+end
+
+def kesha_maker(array)
+  conv = []
+  array.each do |x|
